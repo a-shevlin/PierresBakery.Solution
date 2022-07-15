2691c66356587d2cc989b8ec584fd3b2c10fb360
Alex Shevlin <alexshevlin1@gmail.com>
2022-07-15 10:47:28 -0700

    adjust GetDiscount and _price to pass test 2.

--- Bakery/Models/Pastry.cs
+++ Bakery/Models/Pastry.cs
@@ -4,7 +4,7 @@ namespace BakeryPastry.Model
 {
   public class Pastry
   {
-    private static int _price = 5;
+    private static int _price = 2;
     public int Amount { get; set; }
     public int Price { get; set; }
     
@@ -13,24 +13,24 @@ namespace BakeryPastry.Model
       Amount = int.Parse(input);
     }
 
-    // public int GetPrice()
-    // {
-    //   if (Amount >= 3)
-    //   {
-    //     Price = (Amount * _price) - GetDiscount();
-    //     return Price;
-    //   }
-    //   else
-    //   {
-    //     Price = (Amount * _price);
-    //     return Price;
-    //   }
-    //   return Price;
-    // }
-    // public int GetDiscount()
-    // {                 //3    - (0) / 3 * 5
-    //   int discount = (Amount - (Amount % 3)) / 3 * _price;
-    //   return discount;
-    // }
+    public int GetPrice()
+    {
+      if (Amount >= 3)
+      {
+        Price = (Amount * _price) - GetDiscount();
+        return Price;
+      }
+      else
+      {
+        Price = (Amount * _price);
+        return Price;
+      }
+      return Price;
+    }
+    public int GetDiscount()
+    {                 //3    - (0) / 3 * 5
+      int discount = (Amount - (Amount % 3)) / 3;
+      return discount;
+    }
   }
 }
